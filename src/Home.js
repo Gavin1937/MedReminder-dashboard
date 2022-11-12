f7a5b761f3185c1fde65271b9150d4e85b230fc1
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-11-12 08:46:54 -0800

    finish login screen (#2)

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react'
 import { Navigate } from 'react-router-dom';
-import { Cookies } from "react-cookie";
+import Cookies from 'js-cookie';
 
 class Home extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      // my_info: null,
+      id: null,
+      fname: null,
+      lname: null,
+      phone: null,
+      email: null
+    };
+    
+    this.getMyInfo = this.getMyInfo.bind(this);
+  }
+  
+  getMyInfo = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'username': Cookies.get("username"),
+        'secret': Cookies.get("secret")
+      }
+    };
+    fetch('/api/user/me', requestOptions)
+    .then(response => response.json())
+    .then((data) => {
+      let payload = data.payload;
+      // this.setState({my_info: data.payload})
+      this.setState({
+        id: payload.doc_info.id,
+        fname: payload.doc_info.fname,
+        lname: payload.doc_info.lname,
+        phone: payload.doc_info.phone,
+        email: payload.doc_info.email
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  
+  componentDidMount() {
+    this.getMyInfo();
+  }
+  
   render() {
-    const cookie = new Cookies();
-    let username = cookie.get("username");
-    if (username === null || username === undefined || username === '') {
-      username = sessionStorage.getItem("username");
-    }
-    if (username === null || username === undefined || username === '') {
+    if (Cookies.get("username") === undefined) {
       return (
         <Navigate to="/login" />
       );
     }
     else {
+      // let my_info = this.state.my_info;
       return (
         <div className="Home">
           <h1>Home Page.</h1>
-          <h2>Hello: {username}</h2>
+          <h2>Hello: {this.state.fname+' '+this.state.lname}</h2>
+          <h2>Contact Info</h2>
+          <h3>Phone: {this.state.phone}</h3>
+          <h3>Email: {this.state.email}</h3>
         </div>
       );
     }
@@ -27,4 +71,4 @@ class Home extends Component {
   
 }
 
-export default Home
+export default Home;
--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,62 @@
-import React, { Component } from 'react'
-import { Navigate } from 'react-router-dom';
-import { Cookies } from "react-cookie";
-var md5 = require('md5');
+import React, { Component } from 'react';
+import Cookies from 'js-cookie';
+let md5 = require('md5');
 
 class Login extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      logged_in: false
+      logged_in: false,
+      use_cookie: false,
+      username: null,
+      secret: null,
+      expire: null,
     };
+    
+    // bind functions that requires setState()
+    this.trySetCookies = this.trySetCookies.bind(this);
+    this.login = this.login.bind(this);
   }
   
-  login() {
+  epochToDaysFromNow = (unixTime) => {
+    let now = (new Date()).getTime() / 1000;
+    let output = (unixTime - now) / (3600 * 24);
+    output = output - 0.001;
+    return output;
+  }
+  
+  trySetCookies = async () => {
+    // wait for login() to finish
+    if (this.state.logged_in !== true) {
+      setTimeout(this.trySetCookies, 50);
+      return;
+    }
+    
+    // set cookies & redirect
+    if (this.state.use_cookie === true) {
+      let days = this.epochToDaysFromNow(this.state.expire);
+      Cookies.set("username", this.state.username, { path: '/', expires: days });
+      Cookies.set("secret", this.state.secret, { path: '/', expires: days });
+    }
+    else {
+      Cookies.set("username", this.state.username);
+      Cookies.set("secret", this.state.secret);
+    }
+    window.location.replace('/home');
+  }
+  
+  login = async (e) => {
+    // SUPER IMPORTANT!
+    // this line will force browser to
+    // stuck at this function until it finishes
+    e.preventDefault();
+    
+    // collect ui data & post api auth
     const username = document.querySelector('#uname').value;
     const password = document.querySelector('#pwd').value;
     const auth_hash = md5(username + password);
-    const use_cookie = document.querySelector('#cookie').checked;
-    const cookie = new Cookies();
+    this.setState({use_cookie: document.querySelector('#cookie').checked});
     
     const requestOptions = {
       method: 'POST',
@@ -25,57 +64,38 @@ class Login extends Component {
       body: JSON.stringify({'username': username, 'auth_hash': auth_hash})
     };
     fetch('/api/auth', requestOptions)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      else {
-        document.querySelector('#error').innerHTML = `Fail to login. ${response.json().error}`;
-      }
-    })
-    .then(data => {
+    .then(response => response.json())
+    .then((data) => {
       const payload = data.payload;
-      if (use_cookie === true) {
-        cookie.set("username", payload.username, { path: '/', expires: payload.expire });
-        cookie.set("secret", payload.secret, { path: '/', expires: payload.expire });
-        this.setState({logged_in: true});
-      }
-      else {
-        sessionStorage.setItem("username", payload.username);
-        sessionStorage.setItem("secret", payload.secret);
-        this.setState({logged_in: true});
-      }
-      this.forceUpdate();
+      this.setState({logged_in: true, username: username, secret: payload.secret, expire: payload.expire})
     })
     .catch(error => {
-      console.log(error)
-    })
-  }
+      console.log(error);
+    });
+    
+    // rm ui, set cookies & redirect
+    document.querySelector('.Login').remove()
+    this.trySetCookies()
+    
+  };
   
-  render() {
-    if (this.state.logged_in === true) {
-      return (
-        <Navigate to="/home" />
-      );
-    }
-    else if (this.state.logged_in === false) {
-      return (
-        <div className="Login">
-          <h1>Login Page.</h1>
-          <p id="error" style={{color:"red"}}></p>
-          <form onSubmit={this.login}>
-            <label htmlFor="uname">username</label>
-            <input id="uname" type="text" />
-            <label htmlFor="pwd">password</label>
-            <input id="pwd" type="password" />
-            <label htmlFor="cookie">Use cookie to store login info?</label>
-            <input id="cookie" type="checkbox" />
-            <input type="submit" />
-          </form>
-        </div>
-      );
-    }
-  }
+  render = () => {
+    return (
+      <div className="Login">
+        <h1>Login Page.</h1>
+        <p id="error" style={{color:"red"}}></p>
+        <form onSubmit={this.login}>
+          <label htmlFor="uname">username</label>
+          <input id="uname" type="text" />
+          <label htmlFor="pwd">password</label>
+          <input id="pwd" type="password" />
+          <label htmlFor="cookie">Use cookie to store login info?</label>
+          <input id="cookie" type="checkbox" />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  };
   
 }
 
